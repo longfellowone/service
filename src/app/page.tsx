0bf7227e8c7fea2a327b4da1f94122a91fa31b34
Matt Wright <mattwright3195@gmail.com>
2024-10-24 21:57:11 -0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@ import prisma from "@/lib/prisma";
 export default async function Home() {
   const tasks = await prisma.task.findMany();
 
+  console.log(tasks);
+
   return (
     <div className="">
+      <div>start</div>
       {tasks.map((task) => (
         <div key={task.id}>{task.name}</div>
       ))}
+      <div>end</div>
     </div>
   );
 }
