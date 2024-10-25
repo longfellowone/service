162fd9a65014ae6111b24f51b1569566fc9f79e9
Matt Wright <mattwright3195@gmail.com>
2024-10-24 22:30:31 -0700

    add suspense for dynamic content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import prisma from "@/lib/prisma";
+import { Suspense } from "react";
 
-// Use this unless using suspense to force SSR (not static)
-export const dynamic = "force-dynamic";
+// Use this unless using dynamic APIs or suspense to force SSR (not static)
+// https://nextjs.org/docs/app/building-your-application/data-fetching/fetching
+// https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#reusing-data-across-multiple-functions
+// export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const tasks = await prisma.task.findMany();
@@ -9,10 +12,12 @@ export default async function Home() {
   return (
     <div className="">
       <div>start</div>
-      {tasks.map((task) => (
-        <div key={task.id}>{task.name}</div>
-      ))}
-      <div>end</div>
+      <Suspense fallback={<div>Loading...</div>}>
+        {tasks.map((task) => (
+          <div key={task.id}>{task.name}</div>
+        ))}
+        <div>end</div>
+      </Suspense>
     </div>
   );
 }
