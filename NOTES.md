1a8a79286655bbcfa3fb0a2930ab57e05c5a1048
Matt Wright <mattwright3195@gmail.com>
2024-10-24 18:20:25 -0700

    prisma working

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+Tailwind
+
+- Tailwind fold
+
+Prisma
+
+- npx prisma studio
+- npx prisma db push (use when prototyping)
+- npx prisma migrate (use when developing while in production)
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div className="">Hello world</div>;
+import prisma from "@/lib/prisma";
+
+export default async function Home() {
+  const tasks = await prisma.task.findMany();
+
+  return (
+    <div className="">
+      {tasks.map((task) => (
+        <div key={task.id}>{task.name}</div>
+      ))}
+    </div>
+  );
 }
