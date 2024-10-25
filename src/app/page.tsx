f77e397faba8afc627e866d0f1b1d5517e73a735
Matt Wright <mattwright3195@gmail.com>
2024-10-24 22:22:31 -0700

    fix static render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import prisma from "@/lib/prisma";
 
+// Use this unless using suspense to force SSR (not static)
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const tasks = await prisma.task.findMany();
-
-  console.log(tasks);
 
   return (
     <div className="">
