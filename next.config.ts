0d32af56f7b3f03095137d78ce96acb117375285
Matt Wright <mattwright3195@gmail.com>
2025-05-17 12:16:23 -0700

    updated files and versions

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  experimental: {
+    // ppr: 'incremental',
+    reactCompiler: true,
+  },
+}
+
+export default nextConfig
